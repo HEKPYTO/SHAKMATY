d179b30bd2128898507a0ab1241f9b258dfc026c
J-da <JdaKung@gmail.com>
2024-01-26 18:56:04 +0700

    Add Tester

--- src/test/PieceTest/BishopTest.java
+++ src/test/PieceTest/BishopTest.java
@@ -1,5 +1,26 @@
 package test.PieceTest;
+import game.piece.Bishop;
+import game.position.Position;
+import game.board.Board;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
 
 public class BishopTest {
-    
+
+    @Test
+    public void test_WB() {
+        Bishop W_B = new Bishop(true, new Position(0, 0));
+        Assert.assertEquals(W_B.getWhite(), true); // test get white
+        Assert.assertEquals(W_B.getPos().getX(), 0); // test get X
+        Assert.assertEquals(W_B.getPos().getY(), 0); // test get Y
+    }
+
+    @Test
+    public void test_board() {
+        
+    }
+
 }
--- src/test/PieceTest/PieceTest.java
+++ src/test/PieceTest/PieceTest.java
@@ -1,5 +1,5 @@
 package test.PieceTest;
 
 public class PieceTest {
-    
+
 }
