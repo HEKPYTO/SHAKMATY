798a420ae727278ec4b22411c507b2e697a7ffd6
HE KYPTO <zoonyanapat@gmail.com>
2024-01-27 14:29:16 +0700

    Create BoardTest.java

---
+++ src/test/PieceTest/BoardTest.java
@@ -0,0 +1,20 @@
+package test.PieceTest;
+
+import org.junit.Test;
+
+import game.board.Board;
+
+public class BoardTest {
+
+    Board b;
+    
+    @Test
+    public void boardConstructorTest() {
+
+    }
+
+    @Test
+    public void setPiecePlacement() {
+
+    }
+}
