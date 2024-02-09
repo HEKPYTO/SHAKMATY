b2a87e0ed34bcc7c4e4a7c97993523645e665232
HE KYPTO <zoonyanapat@gmail.com>
2024-02-09 08:58:39 +0700

    still update not done

--- src/game/control/GameLogic.java
+++ src/game/control/GameLogic.java
@@ -3,22 +3,50 @@ package game.control;
 import java.util.ArrayList;
 
 import game.board.Board;
+import game.piece.King;
+import game.piece.Piece;
 import game.position.Position;
 
 public class GameLogic {
 
     private Board current;
-    private ArrayList<Board> hist;
+    private ArrayList<Board> history;
+    private ArrayList<King> wKing;
+    private ArrayList<King> bKing;
     
-    public GameLogic(Board board) {
+    public GameLogic(Board board, boolean isWhite) {
         current = board;
-        hist = new ArrayList<>();
+        history = new ArrayList<>();
+
+        findKing();
     }
 
-    // private ArrayList<Position> findKing() {
+    private void findKing() {
+        for (ArrayList<Piece> col: current.getBoard()) {
+            for (Piece piece: col) {
 
-    // }
+                if (piece instanceof King && piece.isWhite()) {
+                    wKing.add((King) piece);
+                }
+                
+                if (piece instanceof King && !piece.isWhite()) {
+                    bKing.add((King) piece);
+                }
+            }
+        }
+    }
 
+    public void move(Position from, Position to) {
+        
+    }
+
+    public boolean isInCheck() {
+        
+    }
+
+    public boolean isCheckMate() {
+
+    }
 
     public Board getCurrentBoard() {
         return this.current;
@@ -28,12 +56,12 @@ public class GameLogic {
         this.current = current;
     }
 
-    public ArrayList<Board> getHist() {
-        return this.hist;
+    public ArrayList<Board> getHistory() {
+        return this.history;
     }
 
-    public void setHist(ArrayList<Board> hist) {
-        this.hist = hist;
+    public void setHistory(ArrayList<Board> history) {
+        this.history = history;
     }
 
 }
